6ec382b076ac1c6c99e38d89a83955c3171b65f3
annahappyWedding <39616715+annahappyWedding@users.noreply.github.com>
2018-05-26 22:47:25 +0300

    commit
    commit

---
+++ photo-gallery/js/index.js
@@ -0,0 +1,25 @@
+
+
+
+//expands blocks
+var acc = document.getElementsByClassName("expandingBox");
+
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+//lightbox
+lightbox.option({
+		'resizeDuration': 200,
+		'wrapAround': true
+  })
